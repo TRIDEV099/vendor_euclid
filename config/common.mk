d4eefd5c7370c17ef6e134f9c24f5bd0a1eced94
rmp22 <195054967+rmp22@users.noreply.github.com>
2024-06-17 19:55:36 +0900

    config: add blur support
    Change-Id: I5fabdbbdfe812346f17dbba5d4bf9abe7344e0ca
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -36,6 +36,20 @@ endif
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.strictmode.disable=true
 endif
 
+# Enable blur
+TARGET_ENABLE_BLUR ?= true
+ifeq ($(TARGET_ENABLE_BLUR),true)
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.custom.blur.enable=true \
+    persist.sysui.disableBlur=false \
+    ro.surface_flinger.supports_background_blur=1
+else
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.custom.blur.enable=false \
+    persist.sysui.disableBlur=true \
+    ro.surface_flinger.supports_background_blur=0
+endif
+
 # Allow OTA downgrades
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.ota.allow_downgrade=true
