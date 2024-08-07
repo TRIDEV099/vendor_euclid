a65906d63ac87f2005acec1622d718b8a540e8bd
Juhyung Park <qkrwngud825@gmail.com>
2024-08-07 12:44:29 +0000

    SF: set debug.sf.frame_rate_multiple_threshold to 60 by default
    If the device only have 60 and 120 Hz in the config group, 60 Hz won't be
    used when watching a 24 fps video and instead 120 Hz will be used because
    it is perfectly divisible by 24.
    
    This is a publicly documented AOSP behavior [1], but it simply does not
    make any practical sense and trades off significant amount of power for a
    very-hard-to-notice jitter-free experience.
    
    Set debug.sf.frame_rate_multiple_threshold to 60 by default to limit this
    behavior to 60 or lower fps.
    
    [1] https://source.android.com/devices/graphics/multiple-refresh-rate
    
    Change-Id: I66353abc46ad16e63b2c9188794469ad71f2192e
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -16,6 +16,10 @@ PRODUCT_PACKAGES += \
     product_charger_res_images \
     product_charger_res_images_vendor
 
+# Display
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    debug.sf.frame_rate_multiple_threshold=60
+
 # Media
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     media.recorder.show_manufacturer_and_model=true
