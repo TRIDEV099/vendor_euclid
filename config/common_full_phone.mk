e33690e061f289b5c49459b7743d9661cb77727a
Ido Ben-Hur <idoybh2@gmail.com>
2025-07-14 11:38:52 +0300

    config: Enable Bluetooth auto turn on by default
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,8 +1,9 @@
 # Inherit mobile full common euclid stuff
 $(call inherit-product, vendor/euclid/config/common_mobile_full.mk)
 
-# Enable support of one-handed mode
+# Enable support of one-handed mode and BT auto-on
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.support_one_handed_mode?=true
+    ro.support_one_handed_mode?=true \
+    bluetooth.server.automatic_turn_on=true
 
 $(call inherit-product, vendor/euclid/config/telephony.mk)
